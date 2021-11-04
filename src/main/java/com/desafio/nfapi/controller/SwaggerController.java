19c3cddac3380a7dc8355a49bc04118ec5ceef6f
lais211 <lais.sousa1608@gmail.com>
2021-11-04 12:05:23 -0300

    Implementação do swagger

---
+++ src/main/java/com/desafio/nfapi/controller/SwaggerController.java
@@ -0,0 +1,15 @@
+package com.desafio.nfapi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SwaggerController {
+
+    @RequestMapping(value = "/custom", method = RequestMethod.POST)
+    public String custom() {
+        return "custom";
+    }
+}
+
